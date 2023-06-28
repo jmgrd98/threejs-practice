55afc24340c1ac71fc8fbf0052b3be80aaf55ba4
João Marcelo Dantas <jmgrd98@gmail.com>
2023-06-28 09:05:28 -0300

    commit

--- threejs-practice/main.js
+++ threejs-practice/main.js
@@ -1,10 +1,30 @@
-import * as three from 'three'
+import * as three from 'three';
 
-const scene = new three.Scene()
+const scene = new three.Scene();
 
-const geometry = new three.SphereGeometry(3, 64, 64)
+const geometry = new three.SphereGeometry(3, 64, 64);
+const material = new three.MeshStandardMaterial({
+  color: '#00ff83',
+  // roughness: 0.4,
+});
+const mesh = new three.Mesh(geometry, material);
 
-document.querySelector('#app').innerHTML = `
- 
-`
+scene.add(mesh);
 
+const light = new three.PointLight('#ffffff', 1, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+
+const camera = new three.PerspectiveCamera(45, 800 / 600, 0.1, 100);
+camera.position.z = 20;
+scene.add(camera);
+
+const canvas = document.querySelector('#canvas');
+
+const renderer = new three.WebGLRenderer({
+  canvas,
+});
+
+renderer.setSize(800, 600);
+
+renderer.render(scene, camera);
